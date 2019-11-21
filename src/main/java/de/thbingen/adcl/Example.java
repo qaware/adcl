43f010650f1e98a202d607f6bc5dc5ac9e0b4e8b
Nico Eckes <nico.eckes@th-bingen.de>
2019-11-21 01:21:59 +0100

    Added Travis and maven configuration
    Signed-off-by: Nico Eckes <nicoeckes@gmail.com>

--- src/main/java/de/thbingen/adcl/Example.java
+++
@@ -1,7 +0,0 @@
-package de.thbingen.adcl;
-
-public class Example {
-    boolean fun(int a, int b) {
-        return (a > 5) && (b < 15);
-    }
-}
--- src/test/java/de/thbingen/adcl/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Root for test files
- * Tests for subpackages should be placed in equivalent subpackages
- * Test classes and their test methods need to be public!
- */
-package de.thbingen.adcl;
