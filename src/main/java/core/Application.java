a3682b3c3873a50fb08ede90e04945b8022b95de
Bone9 <dani@fblan.de>
2019-12-11 20:13:23 +0100

    Application now loads properties from a file given to the application via commandline.

--- src/main/java/core/Application.java
+++ src/main/java/core/Application.java
@@ -3,6 +3,8 @@ package core;
 import core.information.PackageInformation;
 import database.services.GraphDBService;
 import org.neo4j.ogm.config.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Collection;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -23,34 +26,47 @@ import java.util.stream.Collectors;
 @SpringBootConfiguration
 @ComponentScan(basePackages = "database.*")
 public class Application {
-    public static void main(String[] args) throws IOException {
-        Config.load(args);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static String uri;
+    private static String username;
+    private static String password;
 
-        Path scanLocation = Config.getPath("project.uri", null);
-        if (scanLocation == null) throw new IOException("project.uri is not properly defined in config.properties");
+    public static void main(String[] args) {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(args[0]));
 
-        ClassCollector alg = new ClassCollector(scanLocation.toString());
-        alg.generateFileList();
-        DependencyExtractor extractor = new DependencyExtractor();
+            String destination = properties.getProperty("report.destination");
+            String path = properties.getProperty("project.uri");
+            uri = properties.getProperty("spring.data.neo4j.uri");
+            username = properties.getProperty("spring.data.neo4j.username");
+            password = properties.getProperty("spring.data.neo4j.password");
 
-        Collection<PackageInformation> packages = extractor.analyseClasses(alg.getList().stream()
-                .map(File::getAbsolutePath).collect(Collectors.toList()));
+            ClassCollector alg = new ClassCollector(path);
+            alg.generateFileList();
+            DependencyExtractor extractor = new DependencyExtractor();
 
-        DependencyListWriter.writeListToFile(packages, Config.get("report.destination", "report"), "test");
+            Collection<PackageInformation> packages = extractor.analyseClasses(alg.getList().stream()
+                    .map(File::getAbsolutePath).collect(Collectors.toList()));
 
-        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-        GraphDBService graphDBService = ctx.getBean(GraphDBService.class);
-        graphDBService.saveAllNodes(packages);
-        graphDBService.getPackageRepository().findAll();
-        ctx.close();
+            DependencyListWriter.writeListToFile(packages, destination, "test");
+
+            ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+            GraphDBService graphDBService = ctx.getBean(GraphDBService.class);
+            graphDBService.saveAllNodes(packages);
+            graphDBService.getPackageRepository().findAll();
+            ctx.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
 
     @Bean
     public Configuration configuration() {
         Neo4jProperties properties = new Neo4jProperties();
-        properties.setUri(Config.get("spring.data.neo4j.uri", "bolt://127.0.0.1:7687"));
-        properties.setUsername(Config.get("spring.data.neo4j.username", "neo4j"));
-        properties.setPassword(Config.get("spring.data.neo4j.password", "neo4j"));
+        properties.setUri(uri);
+        properties.setPassword(password);
+        properties.setUsername(username);
         return properties.createConfiguration();
     }
 
