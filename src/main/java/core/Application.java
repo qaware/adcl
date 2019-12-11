e1900fc99a8b62bbe8763df41fc98c5297e3933d
Bone9 <dani@fblan.de>
2019-12-11 20:13:23 +0100

    Application now loads properties from a file given to the application via commandline.

--- src/main/java/core/Application.java
+++ src/main/java/core/Application.java
@@ -2,15 +2,23 @@ package core;
 
 import core.information.PackageInformation;
 import database.services.GraphDBService;
+import org.neo4j.ogm.config.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.neo4j.Neo4jProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -18,23 +26,48 @@ import java.util.stream.Collectors;
 @SpringBootConfiguration
 @ComponentScan(basePackages = "database.*")
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static String uri;
+    private static String username;
+    private static String password;
 
     public static void main(String[] args) {
-        String path = args[1];
-        String destination = args[0];
-        ClassCollector alg = new ClassCollector(path);
-        alg.generateFileList();
-        DependencyExtractor extractor = new DependencyExtractor();
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(args[0]));
 
-        Collection<PackageInformation> packages = extractor.analyseClasses(alg.getList().stream()
-                .map(File::getAbsolutePath).collect(Collectors.toList()));
+            String destination = properties.getProperty("report.destination");
+            String path = properties.getProperty("project.uri");
+            uri = properties.getProperty("spring.data.neo4j.uri");
+            username = properties.getProperty("spring.data.neo4j.username");
+            password = properties.getProperty("spring.data.neo4j.password");
 
-        DependencyListWriter.writeListToFile(packages, destination, "test");
+            ClassCollector alg = new ClassCollector(path);
+            alg.generateFileList();
+            DependencyExtractor extractor = new DependencyExtractor();
 
-        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-        GraphDBService graphDBService = ctx.getBean(GraphDBService.class);
-        graphDBService.saveAllNodes(packages);
-        graphDBService.getPackageRepository().findAll();
-        ctx.close();
+            Collection<PackageInformation> packages = extractor.analyseClasses(alg.getList().stream()
+                    .map(File::getAbsolutePath).collect(Collectors.toList()));
+
+            DependencyListWriter.writeListToFile(packages, destination, "test");
+
+            ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+            GraphDBService graphDBService = ctx.getBean(GraphDBService.class);
+            graphDBService.saveAllNodes(packages);
+            graphDBService.getPackageRepository().findAll();
+            ctx.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
+
+    @Bean
+    public Configuration configuration() {
+        Neo4jProperties properties = new Neo4jProperties();
+        properties.setUri(uri);
+        properties.setPassword(password);
+        properties.setUsername(username);
+        return properties.createConfiguration();
+    }
+
 }
