6796006f6b8a71dadc8b42fbd5c1fbb5792b3560
Ramazan Dinlemez <ramazan.dinlemez@qaware.de>
2019-12-12 16:19:15 +0100

    fix/1.0.4 Resolve NotFoundException then method parameter list contains external classes

--- src/main/java/core/CtBehaviorBodyAnalyzer.java
+++ src/main/java/core/CtBehaviorBodyAnalyzer.java
@@ -4,18 +4,20 @@ import core.information.BehaviorInformation;
 import core.information.ClassInformation;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
-import javassist.CtMethod;
-import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 import javassist.expr.NewExpr;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * The  CtBehaviorBodyAnalyzer is used to extract information about with classes and methods are referenced inside a method/constructor body.
+ * The CtBehaviorBodyAnalyzer is used to extract information about with classes and methods are referenced inside a method/constructor body.
  */
 public class CtBehaviorBodyAnalyzer extends ExprEditor {
 
@@ -37,19 +39,10 @@ public class CtBehaviorBodyAnalyzer extends ExprEditor {
      *
      * @param ctBehaviour the ct behaviour
      * @throws CannotCompileException if CtBehavior body cannot be compiled
-     * @throws NotFoundException      parameterTypes could not be found
      */
-    void analyse(CtBehavior ctBehaviour) throws CannotCompileException, NotFoundException {
+    void analyse(CtBehavior ctBehaviour) throws CannotCompileException {
         ctBehaviour.instrument(this);
-        Arrays.stream(ctBehaviour.getParameterTypes()).forEach(ctClass -> referencedClasses.add(dependencyPool.getOrCreateClassInformation(ctClass.getName())));
-
-        if (ctBehaviour instanceof CtMethod) {
-            CtMethod ctMethod = (CtMethod) ctBehaviour;
-            String returnType = ctMethod.getReturnType().getName();
-            if (!returnType.equals("void")) {
-                referencedClasses.add(dependencyPool.getOrCreateClassInformation(returnType));
-            }
-        }
+        addParameterTypesAsDependencies(ctBehaviour.getSignature());
     }
 
     @Override
@@ -67,14 +60,43 @@ public class CtBehaviorBodyAnalyzer extends ExprEditor {
     }
 
     /**
-     * Parses the jvm style into a more readable format
+     * Parses the jvm style into a list of the contained types.
      *
      * @param signature the jvm style signature
-     * @return formatted signature
+     * @return list of types
+     */
+    private List<String> parseSignatureIntoParameterTypes(String signature) {
+        String[] split = signature.replace("/", ".").replace("(", "").replace(")", "").split(";");
+
+        Pattern pattern = Pattern.compile("[a-z]");
+        List<String> formatted = new ArrayList<>();
+        Arrays.stream(split).forEach(s -> {
+            Matcher matcher = pattern.matcher(s);
+            if (matcher.find()) {
+                formatted.add(s.substring(matcher.start()));
+            }
+        });
+        return formatted;
+    }
+
+    /**
+     * Parses a jvm style signature into a Method parameter list.
+     *
+     * @param signature the jvm style signature
+     * @return types in style of (Type,Type,Type,...)
      */
     private String parseSignature(String signature) {
-        return signature.replace("(L", "(").replace("/", ".").replace(";L", ",")
-                .replace(";)Z", ")").replace(";)V", ")").replace(")V", ")");
+        return parseSignatureIntoParameterTypes(signature.substring(signature.indexOf('('), signature.lastIndexOf(')'))).toString().replace("[", "(").replace("]", ")").replace(" ", "");
+    }
+
+    /**
+     * Adds the Parameter types to the references Classes.
+     *
+     * @param signature in JVM Type signature
+     */
+    private void addParameterTypesAsDependencies(String signature) {
+        List<String> parameterTypes = parseSignatureIntoParameterTypes(signature);
+        parameterTypes.forEach(parameterType -> referencedClasses.add(dependencyPool.getOrCreateClassInformation(parameterType)));
     }
 
     /**
