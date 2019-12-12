3f0c2c60194a999232c4d71cc3bc5f6e1d3c04f0
Nico Eckes <nicoeckes@gmail.com>
2019-12-13 00:48:00 +0100

    Externalized Configuration to Config class
    Signed-off-by: Nico Eckes <nicoeckes@gmail.com>

--- src/main/java/core/Application.java
+++ src/main/java/core/Application.java
@@ -3,8 +3,6 @@ package core;
 import core.information.PackageInformation;
 import database.services.GraphDBService;
 import org.neo4j.ogm.config.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Collection;
-import java.util.Properties;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -26,47 +21,31 @@ import java.util.stream.Collectors;
 @SpringBootConfiguration
 @ComponentScan(basePackages = "database.*")
 public class Application {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-    private static String uri;
-    private static String username;
-    private static String password;
-
     public static void main(String[] args) {
-        try {
-            Properties properties = new Properties();
-            properties.load(new FileInputStream(args[0]));
+        Config.load(args);
 
-            String destination = properties.getProperty("report.destination");
-            String path = properties.getProperty("project.uri");
-            uri = properties.getProperty("spring.data.neo4j.uri");
-            username = properties.getProperty("spring.data.neo4j.username");
-            password = properties.getProperty("spring.data.neo4j.password");
+        ClassCollector alg = new ClassCollector(Config.get("project.uri", "."));
+        alg.generateFileList();
+        DependencyExtractor extractor = new DependencyExtractor();
 
-            ClassCollector alg = new ClassCollector(path);
-            alg.generateFileList();
-            DependencyExtractor extractor = new DependencyExtractor();
+        Collection<PackageInformation> packages = extractor.analyseClasses(alg.getList().stream()
+                .map(File::getAbsolutePath).collect(Collectors.toList()));
 
-            Collection<PackageInformation> packages = extractor.analyseClasses(alg.getList().stream()
-                    .map(File::getAbsolutePath).collect(Collectors.toList()));
+        DependencyListWriter.writeListToFile(packages, Config.get("report.destination", "report"), "test");
 
-            DependencyListWriter.writeListToFile(packages, destination, "test");
-
-            ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
-            GraphDBService graphDBService = ctx.getBean(GraphDBService.class);
-            graphDBService.saveAllNodes(packages);
-            graphDBService.getPackageRepository().findAll();
-            ctx.close();
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+        GraphDBService graphDBService = ctx.getBean(GraphDBService.class);
+        graphDBService.saveAllNodes(packages);
+        graphDBService.getPackageRepository().findAll();
+        ctx.close();
     }
 
     @Bean
     public Configuration configuration() {
         Neo4jProperties properties = new Neo4jProperties();
-        properties.setUri(uri);
-        properties.setPassword(password);
-        properties.setUsername(username);
+        properties.setUri(Config.get("spring.data.neo4j.uri", "bolt://127.0.0.1:7687"));
+        properties.setUsername(Config.get("spring.data.neo4j.username", "neo4j"));
+        properties.setPassword(Config.get("spring.data.neo4j.password", "neo4j"));
         return properties.createConfiguration();
     }
 
