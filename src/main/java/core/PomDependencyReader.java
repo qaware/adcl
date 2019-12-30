eb7cef95de349b49a8b216a05cce10913e352deb
t <torben.jox@t-online.de>
2019-12-30 10:53:12 +0100

    geändert:       src/main/java/core/PomDependencyReader.java

--- src/main/java/core/PomDependencyReader.java
+++ src/main/java/core/PomDependencyReader.java
@@ -3,13 +3,14 @@ package core;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
-import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.*;
 
 public class PomDependencyReader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PomDependencyReader.class);
     MavenXpp3Reader reader;
     Model model;
     InputStream in;
@@ -19,15 +20,22 @@ public class PomDependencyReader {
         model = null;
         in = new FileInputStream("pom.xml");
     }
-    public List<Dependency> readDependency() throws IOException {
-        InputStream in = new FileInputStream("pom.xml");
-        List<Dependency> list = model.getDependencies();
-        return list;
-    }
-    public void close() throws IOException {
-        in.close();
+    public List<Dependency> readDependency() {
+        try {
+            model = reader.read(in);
+            return model.getDependencies();
+        }
+        catch(Exception ex){
+            LOGGER.info(ex.getMessage());
+        }
+        return new ArrayList<>();
     }
     public void printListDependency(){
-
+        try {
+            readDependency().forEach(dependency -> LOGGER.info(dependency.toString()));
+        }
+        catch(Exception ex){
+            LOGGER.info(ex.getMessage());
+        }
     }
 }
