1c22582103693cba3d54a7c51d0c5d6d32a8f150
Nico Eckes <nicoeckes@gmail.com>
2020-01-08 19:22:00 +0100

    Removed Information Comparators as they are Comparable, moved isInternal to classes
    Fixed hashCode, toString, equals and compareTo methods in
    - PackageInformation
    - ClassInformation
    - MethodInformation
    
    Removed isConstructor
    Constructors now have .<init>() method name

--- src/main/java/core/information/MethodInformation.java
+++ src/main/java/core/information/MethodInformation.java
@@ -4,8 +4,10 @@ import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
+import util.Utils;
 
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -27,25 +29,15 @@ public class MethodInformation implements Comparable<MethodInformation> {
     @Relationship(type = "USES")
     private SortedSet<MethodInformation> methodDependencies;
 
-    private boolean isConstructor;
-
-    @Override
-    public String toString() {
-        return ("Method " + name + " (id=" + id + ", " + (isConstructor ? "constructor" : "no-constructor") + ") {\n"
-                + "packageDependencies=[" + packageDependencies.stream().map(PackageInformation::getPackageName).collect(Collectors.joining(", ")) + "]\n"
-                + "classDependencies=[" + classDependencies.stream().map(ClassInformation::getClassName).collect(Collectors.joining(", ")) + "]\n"
-                + "methodDependencies=[" + methodDependencies.stream().map(MethodInformation::getName).collect(Collectors.joining(", ")) + "]"
-        ).replace("\n", "\n    ") + "\n}";
-    }
-
     /**
      * Instantiates a new Method information.
      *
      * @param name          the name of the method
      * @param isConstructor true if this method is a constructor
      */
+    @Deprecated
     public MethodInformation(String name, boolean isConstructor) {
-        this(name, new TreeSet<>(PackageInformation.PackageInformationComparator.getInstance()), new TreeSet<>(ClassInformation.ClassInformationComparator.getInstance()), new TreeSet<>(MethodInformationComparator.getInstance()), isConstructor);
+        this(name, new TreeSet<>(), new TreeSet<>(), new TreeSet<>());
     }
 
     /**
@@ -57,12 +49,36 @@ public class MethodInformation implements Comparable<MethodInformation> {
      * @param methodDependencies   the referenced method
      * @param isConstructor      true if method is constructor
      */
+    @Deprecated
     public MethodInformation(String name, SortedSet<PackageInformation> packageDependencies, SortedSet<ClassInformation> classDependencies, SortedSet<MethodInformation> methodDependencies, boolean isConstructor) {
         this.name = name;
         this.packageDependencies = packageDependencies;
         this.classDependencies = classDependencies;
         this.methodDependencies = methodDependencies;
-        this.isConstructor = isConstructor;
+    }
+
+    /**
+     * Instantiates a new Method information.
+     *
+     * @param name the name of the method
+     */
+    public MethodInformation(String name) {
+        this(name, new TreeSet<>(), new TreeSet<>(), new TreeSet<>());
+    }
+
+    /**
+     * Instantiates a new Method information.
+     *
+     * @param name                the name of the method
+     * @param packageDependencies the referenced packages
+     * @param classDependencies   the referenced classes
+     * @param methodDependencies  the referenced method
+     */
+    public MethodInformation(String name, SortedSet<PackageInformation> packageDependencies, SortedSet<ClassInformation> classDependencies, SortedSet<MethodInformation> methodDependencies) {
+        this.name = name;
+        this.packageDependencies = packageDependencies;
+        this.classDependencies = classDependencies;
+        this.methodDependencies = methodDependencies;
     }
 
     /**
@@ -114,49 +130,35 @@ public class MethodInformation implements Comparable<MethodInformation> {
     }
 
     public boolean isConstructor() {
-        return isConstructor;
+        return name.indexOf('<') != -1;
     }
 
     @Override
     public int compareTo(MethodInformation methodInformation) {
-        return MethodInformationComparator.getInstance().compare(this, methodInformation);
+        return Comparator.comparing(MethodInformation::isConstructor)
+                .thenComparing(MethodInformation::getName)
+                .thenComparing(MethodInformation::getPackageDependencies, Utils.sortedSetComparator(Comparator.comparing(PackageInformation::getPackageName)))
+                .thenComparing(MethodInformation::getClassDependencies, Utils.sortedSetComparator(Comparator.comparing(ClassInformation::getClassName)))
+                .thenComparing(MethodInformation::getMethodDependencies, Utils.sortedSetComparator(Comparator.comparing(MethodInformation::getName)))
+                .compare(this, methodInformation);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof MethodInformation) {
-            MethodInformation methodInformation = (MethodInformation) obj;
-            return methodInformation.compareTo(this) == 0 && isConstructor == methodInformation.isConstructor();
-        }
-        return false;
+        return obj instanceof MethodInformation && ((MethodInformation) obj).compareTo(this) == 0;
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return Objects.hash(name);
     }
 
-    /**
-     * Comparator for the type MethodInformation based on the method name.
-     */
-    public static class MethodInformationComparator implements Comparator<MethodInformation> {
-
-        private static MethodInformationComparator instance;
-
-        public static MethodInformationComparator getInstance() {
-            if (instance == null) {
-                instance = new MethodInformationComparator();
-            }
-            return instance;
-        }
-
-        private MethodInformationComparator() {
-        }
-
-        @Override
-        public int compare(MethodInformation methodInformation, MethodInformation otherMethodInformation) {
-            return methodInformation.name.compareTo(otherMethodInformation.name);
-        }
+    @Override
+    public String toString() {
+        return ("Method " + name + " (id=" + id + ", " + (isConstructor() ? "constructor" : "no-constructor") + ") {\n"
+                + "packageDependencies=[" + packageDependencies.stream().map(PackageInformation::getPackageName).collect(Collectors.joining(", ")) + "]\n"
+                + "classDependencies=[" + classDependencies.stream().map(ClassInformation::getClassName).collect(Collectors.joining(", ")) + "]\n"
+                + "methodDependencies=[" + methodDependencies.stream().map(MethodInformation::getName).collect(Collectors.joining(", ")) + "]"
+        ).replace("\n", "\n    ") + "\n}";
     }
 }
-
