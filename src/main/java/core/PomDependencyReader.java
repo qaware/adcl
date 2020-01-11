3a4aced3890fa42abd224f46ca8d6a471e4d89e9
jod32 <torben.jox@t-online.de>
2020-01-11 12:07:15 +0100

    geändert:       src/main/java/core/PomDependencyReader.java

--- src/main/java/core/PomDependencyReader.java
+++ src/main/java/core/PomDependencyReader.java
@@ -3,6 +3,7 @@ package core;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,9 +19,8 @@ public class PomDependencyReader {
     /**
      * Init PomDependencyReader
      * @param path Path describes where the pom.xml file is
-     * @throws FileNotFoundException
      */
-    public PomDependencyReader(String path) throws FileNotFoundException {
+    public PomDependencyReader(String path) {
         this.path = path;
     }
 
@@ -28,16 +28,18 @@ public class PomDependencyReader {
      * Reads all dependencies from pom.xml file
      * @return returns an arraylist with dependencies
      */
-    public List<Dependency> readDependency() {
+    public Set<Dependency> readDependency() {
         MavenXpp3Reader reader = new MavenXpp3Reader();
         try {
             Model model = reader.read(new FileReader(path));
-            return model.getDependencies();
+            Set<Dependency> dependencies = new TreeSet<Dependency>();
+            dependencies.addAll(model.getDependencies());
+            return dependencies;
         }
         catch(XmlPullParserException | IOException ex) {
             LOGGER.info(ex.getMessage());
         }
-        return new ArrayList<>();
+        return null;
     }
 
     /**
