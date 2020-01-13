f6631416ba25481d2d8959f63317827d6b382d0e
Nico Eckes <nicoeckes@gmail.com>
2020-01-13 19:32:09 +0100

    Extended test for different inputs and quoted values
    Took 2 hours 18 minutes

--- src/test/java/core/ConfigTest.java
+++ src/test/java/core/ConfigTest.java
@@ -1,6 +1,6 @@
 package core;
 
-import org.junit.jupiter.api.AfterAll;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,42 +8,41 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ConfigTest {
-    private static final Path confPath = Paths.get("myconf.properties");
+    private static Properties defaultProps;
 
     @BeforeAll
-    static void initConfig() throws IOException {
-        Files.write(confPath, Collections.singletonList("a=Hello World\nb=3\ne=."));
-        Config.load(new String[]{"configPath=" + confPath, "c", "d=a,b,c"});
-    }
-
-    @AfterAll
-    static void cleanup() throws IOException {
-        Files.delete(confPath);
+    static void beforeAll() {
+        defaultProps = System.getProperties();
     }
 
     @Test
-    void testImplicitTrue() {
-        assertTrue(Config.get("c", false));
-    }
+    void testGetExistent() {
+        configByArgs(
+                Pair.of("a", "Hello World"),
+                Pair.of("b", 3),
+                Pair.of("c", true),
+                Pair.of("d", "a,b,c"),
+                Pair.of("e", ".")
+        );
 
-    @Test
-    void testNumbers() {
+        assertTrue(Config.valuePresent("a"));
+
         assertEquals(Config.get("b", 0), 3);
         assertEquals(Config.get("b", (long) 0), 3);
         assertEquals(Config.get("b", (float) 0), 3);
         assertEquals(Config.get("b", (double) 0), 3);
         assertEquals(Config.get("b", (byte) 0), 3);
         assertEquals(Config.get("b", (short) 0), 3);
-    }
 
-    @Test
-    void testOther() {
+        assertTrue(Config.get("c", false));
         assertEquals(Config.get("b", '0'), '3');
         assertEquals(Config.get("a", null), "Hello World");
         assertThat(Config.getStringList("d", null)).containsExactly("a", "b", "c");
@@ -51,9 +50,133 @@ public class ConfigTest {
     }
 
     @Test
-    void testDefault() {
-        assertThat(Config.get("x", "A")).isEqualTo("A");
-        assertNull(Config.get(null, null));
-        assertEquals(Config.get("a", 0), 0);
+    void testGetWrongData() {
+        configByArgs(
+                Pair.of("b", "x"),
+                Pair.of("c", 3),
+                Pair.of("e", "???")
+        );
+
+        assertEquals(Config.get("b", 4), 4);
+        assertEquals(Config.get("b", (long) 4), 4);
+        assertEquals(Config.get("b", (float) 4), 4);
+        assertEquals(Config.get("b", (double) 4), 4);
+        assertEquals(Config.get("b", (byte) 4), 4);
+        assertEquals(Config.get("b", (short) 4), 4);
+
+        assertTrue(Config.get("c", true));
+        assertEquals(Config.get("e", '4'), '4');
+        assertNull(Config.getPath("e", null));
+    }
+
+    @Test
+    void testGetNotExistent() {
+        configByArgs();
+
+        assertFalse(Config.valuePresent("a"));
+
+        assertEquals(Config.get("b", 4), 4);
+        assertEquals(Config.get("b", (long) 4), 4);
+        assertEquals(Config.get("b", (float) 4), 4);
+        assertEquals(Config.get("b", (double) 4), 4);
+        assertEquals(Config.get("b", (byte) 4), 4);
+        assertEquals(Config.get("b", (short) 4), 4);
+
+        assertTrue(Config.get("c", true));
+        assertEquals(Config.get("b", '4'), '4');
+        assertEquals(Config.get("a", "X"), "X");
+        assertNull(Config.getStringList("d", null));
+        assertNull(Config.getPath("e", null));
+    }
+
+    @Test
+    void testGetNoValue() {
+        configByArgs(Pair.of("a", null));
+
+        assertTrue(Config.valuePresent("a"));
+        assertTrue(Config.get("a", false));
+    }
+
+    @Test
+    void testInputArgs() {
+        String input = "a=1 b= c d=\"3\" e=\"a b\"";
+        Config.load(input.split(" "));
+
+        assertEquals(Config.get("a", 0), 1);
+        assertTrue(Config.get("b", false));
+        assertTrue(Config.get("c", false));
+        assertEquals(Config.get("d", 0), 3);
+        assertEquals(Config.get("e", null), "a b");
+    }
+
+    @Test
+    void testInputProperties() {
+        System.setProperty("adcl.a", "1");
+        System.setProperty("adcl.b", "");
+        System.setProperty("adcl.d", "3");
+        System.setProperty("adcl.e", "a b");
+        Config.load(new String[0]);
+
+        assertEquals(Config.get("a", 0), 1);
+        assertTrue(Config.get("b", false));
+        assertEquals(Config.get("d", 0), 3);
+        assertEquals(Config.get("e", null), "a b");
+
+        System.setProperties(defaultProps);
+    }
+
+    @Test
+    void testInputFilePathGiven() throws IOException {
+        Path path = Paths.get("myconf.properties");
+        Files.write(path, Arrays.asList(
+                "a=1",
+                "b=",
+                "c",
+                "d=\"3\"",
+                "e=\"a b\""
+        ));
+
+        Config.load(new String[]{"configPath=myconf.properties"});
+
+        assertEquals(Config.get("a", 0), 1);
+        assertTrue(Config.get("b", false));
+        assertTrue(Config.get("c", false));
+        assertEquals(Config.get("d", 0), 3);
+        assertEquals(Config.get("e", null), "a b");
+
+        Files.delete(path);
+    }
+
+    @Test
+    void testInputFileDefaultPath() throws IOException {
+        Path path = Paths.get("config.properties");
+        Files.write(path, Arrays.asList(
+                "a=1",
+                "b=",
+                "c",
+                "d=\"3\"",
+                "e=\"a b\""
+        ));
+
+        Config.load(new String[0]);
+
+        assertEquals(Config.get("a", 0), 1);
+        assertTrue(Config.get("b", false));
+        assertTrue(Config.get("c", false));
+        assertEquals(Config.get("d", 0), 3);
+        assertEquals(Config.get("e", null), "a b");
+
+        Files.delete(path);
+    }
+
+    @SafeVarargs
+    private final void configByArgs(Pair<String, Object>... pairs) {
+        String args = Arrays.stream(pairs).map(p -> {
+            Object valRaw = p.getValue();
+            String val = valRaw == null ? "" : String.valueOf(valRaw);
+            if (val.contains(" ")) val = '"' + val + '"';
+            return p.getKey() + '=' + val;
+        }).collect(Collectors.joining(" "));
+        Config.load(args.split(" "));
     }
 }
