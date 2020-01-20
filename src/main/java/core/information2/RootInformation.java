2a29a8d654e72d5d788fdc63bf49816b9bed6b33
Nico Eckes <nicoeckes@gmail.com>
2020-01-20 13:46:39 +0100

    Added RootInformation

---
+++ src/main/java/core/information2/RootInformation.java
@@ -0,0 +1,33 @@
+package core.information2;
+
+import org.jetbrains.annotations.NotNull;
+import org.neo4j.ogm.annotation.Property;
+
+public class RootInformation extends Information<RootInformation> {
+    @Property
+    private final int modelVersion = 2;
+
+    public RootInformation(@NotNull RootInformation parent) {
+        super(parent, "");
+    }
+
+    public int getModelVersion() {
+        return modelVersion;
+    }
+
+    @Override
+    public @NotNull Type getType() {
+        return Type.ROOT;
+    }
+
+    @NotNull
+    @Override
+    public RootInformation getParent() {
+        return this;
+    }
+
+    @Override
+    public boolean exists(VersionInformation version) {
+        return true;
+    }
+}
