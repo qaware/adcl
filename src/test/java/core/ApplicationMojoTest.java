af63cccaf63150f2291e5975964e4a57ba83f15f
Nico Eckes <nicoeckes@gmail.com>
2020-01-20 14:11:02 +0100

    Fixed test

--- src/test/java/core/ApplicationMojoTest.java
+++ src/test/java/core/ApplicationMojoTest.java
@@ -1,11 +1,22 @@
 package core;
+
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
 import java.io.File;
 
 public class ApplicationMojoTest extends AbstractMojoTestCase {
     public void testMojoGoal() throws Exception {
         File testPom = new File(getBasedir(), "/src/test/resources/pom2/pom.xml");
 
-        ApplicationMojo mojo = (ApplicationMojo) lookupMojo("start", testPom);
+        Exception e = null;
+        try {
+            ApplicationMojo mojo = (ApplicationMojo) lookupMojo("start", testPom);
+            mojo.execute();
+        } catch (MojoExecutionException ex) {
+            e = ex;
+        }
+        assertNotNull(e);
+        assertEquals(e.getCause().getMessage(), "project.uri is not properly defined in config.properties");
     }
 }
