63ebed94d771d6e5e773f80b3071a8737747fa7e
Bone9 <dani@fblan.de>
2020-01-22 00:06:09 +0100

    Feature 1.11.2 ApplicationMojo is now fully configurable.

--- src/main/java/core/ApplicationMojo.java
+++ src/main/java/core/ApplicationMojo.java
@@ -5,21 +5,24 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * ApplicationMojo is the entry point for maven goal {@code adcl:start}.
+ * Goal is to start the Application with the arguments given via Maven in the configuration tag.
+ */
 @Mojo(name = "start")
 public class ApplicationMojo extends AbstractMojo {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMojo.class);
+
     @Parameter
     private Properties properties;
+
     @Override
     public void execute() throws MojoExecutionException {
         try {
-            System.setProperties(properties);
+            properties.forEach((key, value) -> System.setProperty("adcl." + key.toString(), value.toString()));
             Application.main(new String[0]);
         } catch (IOException e) {
             throw new MojoExecutionException("Error while executing ADCL application", e);
--- src/test/java/core/ApplicationMojoErrorFreeInternalTest.java
+++ src/test/java/core/ApplicationMojoErrorFreeInternalTest.java
@@ -3,10 +3,30 @@ package core;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
 public class ApplicationMojoErrorFreeInternalTest extends AbstractMojoTestCase {
+
     public void test() throws Exception {
         ApplicationMojo mojo = (ApplicationMojo) lookupMojo("start", "src/test/resources/pom3/pom.xml");
         ApplicationMojo mojo2 = (ApplicationMojo) lookupMojo("start", "src/test/resources/pom4/pom.xml");
-        mojo.execute();
-        mojo2.execute();
+
+        try {
+            mojo.execute();
+        } catch (Exception ignored) {
+        }
+        assertEquals(Config.get("spring.data.neo4j.uri", "NA"), "bolt://localhost:7687");
+        assertEquals(Config.get("spring.data.neo4j.username", "NA"), "neo4j");
+        assertEquals(Config.get("spring.data.neo4j.password", "NA"), "test");
+        assertEquals(Config.get("project.commit.current", "NA"), "test");
+        assertEquals(Config.get("project.uri", "NA"), "src/test/resources/test classfiles3/epro1");
+
+        try {
+            mojo2.execute();
+        } catch (Exception ignored) {
+        }
+        assertEquals(Config.get("spring.data.neo4j.uri", "NA"), "bolt://localhost:7687");
+        assertEquals(Config.get("spring.data.neo4j.username", "NA"), "neo4j\"");
+        assertEquals(Config.get("spring.data.neo4j.password", "NA"), "test");
+        assertEquals(Config.get("project.commit.current", "NA"), "test2");
+        assertEquals(Config.get("project.commit.previous", "NA"), "test");
+        assertEquals(Config.get("project.uri", "NA"), "src\\test\\resources\\testclassfiles3\\epro2");
     }
 }
