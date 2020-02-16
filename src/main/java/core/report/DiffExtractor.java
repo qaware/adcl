6c316eb89c17b068eaca48b2b2620acc934ff122
Nico Eckes <nicoeckes@gmail.com>
2020-02-16 14:51:45 +0100

    Added javadoc, generalized DependencyEntry constructors

--- src/main/java/core/report/DiffExtractor.java
+++ src/main/java/core/report/DiffExtractor.java
@@ -16,19 +16,35 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Generates a list of differences between two versions of a project
+ */
 public class DiffExtractor {
     @Nullable
     private final VersionInformation from;
     @NotNull
     private final VersionInformation to;
 
+    /**
+     * Creates a new diff extractor. Does not take version order into consideration
+     *
+     * @param from the start version to create a diff
+     * @param to   the end version to create a diff
+     */
     public DiffExtractor(@Nullable VersionInformation from, @NotNull VersionInformation to) {
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * @param info              the information of whose parents should be listed
+     * @param at                the version to create the dependency set
+     * @param aggregateDepStart whether a dependency starting at a level should be displayed for higher levels
+     * @param aggregateDepEnd   whether a dependency ending at a level should be displayed for higher levels
+     * @return all dependencies starting at this node or at one of its children
+     */
     @NotNull
-    private Stream<DependencyEntry> generateDependencySet(VersionInformation at, @NotNull Information<?> info, boolean aggregateDepStart, boolean aggregateDepEnd) {
+    private static Stream<DependencyEntry> generateDependencySet(@NotNull Information<?> info, VersionInformation at, boolean aggregateDepStart, boolean aggregateDepEnd) {
         Set<Information<?>> deps = Utils.concatStreams(
                 info.getMethodDependencies(at).stream(),
                 info.getClassDependencies(at).stream(),
@@ -36,24 +52,45 @@ public class DiffExtractor {
                 info.getProjectDependencies(at).stream()
         ).collect(Collectors.toSet());
 
-        Stream<DependencyEntry> ownEntries = deps.stream().map(i -> new DependencyEntry(false, info, i)); // original dependencies
-        if (aggregateDepEnd)
-            ownEntries = Stream.concat(ownEntries, deps.stream().flatMap(this::allParents).map(i -> new DependencyEntry(true, info, i))); // aggregated end notes
-        Stream<DependencyEntry> childEntries = info.getDirectChildren(at).stream().flatMap(i -> generateDependencySet(at, i, aggregateDepStart, aggregateDepEnd)); // children
+        Stream<DependencyEntry> ownEntries = deps.stream().map(i -> // original dependencies
+                new DependencyEntry(new DependencyEntry.DependencyNode(false, info), new DependencyEntry.DependencyNode(false, i))
+        );
+
+        if (aggregateDepEnd) ownEntries = Stream.concat(ownEntries, // aggregated end notes
+                deps.stream().flatMap(DiffExtractor::allParents).map(i ->
+                        new DependencyEntry(new DependencyEntry.DependencyNode(false, info), new DependencyEntry.DependencyNode(true, i))
+                )
+        );
+
+        Stream<DependencyEntry> childEntries = info.getDirectChildren(at).stream().flatMap(i -> // children
+                generateDependencySet(i, at, aggregateDepStart, aggregateDepEnd)
+        );
+
         if (aggregateDepStart)
-            childEntries = childEntries.flatMap(e -> Stream.of(e, new DependencyEntry(info, e))); // children with aggregated start nodes
+            childEntries = childEntries.flatMap(e -> Stream.of(e, // children with aggregated start nodes
+                    new DependencyEntry(new DependencyEntry.DependencyNode(true, info), new DependencyEntry.DependencyNode(e, true))
+            ));
 
         return Stream.concat(ownEntries, childEntries);
     }
 
-    private Stream<Information<?>> allParents(@NotNull Information<?> info) {
+    /**
+     * @param info the information of whose parents should be listed
+     * @return All parent information of parameter 'info', self not included, root not included
+     */
+    private static Stream<Information<?>> allParents(@NotNull Information<?> info) {
         return info.getParent().getType() == Information.Type.ROOT ? Stream.empty() : Stream.concat(allParents(info.getParent()), Stream.of(info.getParent()));
     }
 
+    /**
+     * @param aggregateDepStart whether a dependency starting at a level should be displayed for higher levels
+     * @param aggregateDepEnd   whether a dependency ending at a level should be displayed for higher levels
+     * @return a set describing the differences between the specified versions in constructor
+     */
     @NotNull
     public Set<DependencyEntry> generateChangelist(boolean aggregateDepStart, boolean aggregateDepEnd) {
-        Set<DependencyEntry> before = from == null ? Collections.emptySet() : generateDependencySet(from, to.getProject(), aggregateDepStart, aggregateDepEnd).collect(Collectors.toSet());
-        Set<DependencyEntry> after = generateDependencySet(to, to.getProject(), aggregateDepStart, aggregateDepEnd).collect(Collectors.toSet());
+        Set<DependencyEntry> before = from == null ? Collections.emptySet() : generateDependencySet(to.getProject(), from, aggregateDepStart, aggregateDepEnd).collect(Collectors.toSet());
+        Set<DependencyEntry> after = generateDependencySet(to.getProject(), to, aggregateDepStart, aggregateDepEnd).collect(Collectors.toSet());
         Set<DependencyEntry> removed = new HashSet<>(before);
         removed.removeAll(after);
         Set<DependencyEntry> added = new HashSet<>(after);
@@ -63,15 +100,24 @@ public class DiffExtractor {
         return removed;
     }
 
+    /**
+     * @param aggregateDepStart whether a dependency starting at a level should be displayed for higher levels
+     * @param aggregateDepEnd   whether a dependency ending at a level should be displayed for higher levels
+     * @return a json serialized form of {@link DiffExtractor#generateChangelist(boolean, boolean)}
+     * @throws JsonProcessingException on json generation failure
+     * @see DiffExtractor#generateChangelist(boolean, boolean)
+     */
     public String generateJson(boolean aggregateDepStart, boolean aggregateDepEnd) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(generateChangelist(aggregateDepStart, aggregateDepEnd));
     }
 
+    /**
+     * Serialization class
+     * syntheticStart / syntheticEnd describe whether this dependency entry's start / end node represents the actual dependency or represents an aggregated version of another dependency entry
+     */
     public static class DependencyEntry {
         @JsonProperty("syntheticStart")
         public final boolean syntheticStart;
-        @JsonProperty("syntheticEnd")
-        public final boolean syntheticEnd;
         @JsonProperty("usedByType")
         @NotNull
         public final Information.Type startType;
@@ -81,6 +127,8 @@ public class DiffExtractor {
         @JsonProperty("usedByName")
         @NotNull
         public final String startName;
+        @JsonProperty("syntheticEnd")
+        public final boolean syntheticEnd;
         @JsonProperty("dependencyType")
         @NotNull
         public final Information.Type endType;
@@ -93,27 +141,15 @@ public class DiffExtractor {
         @JsonProperty("changeStatus")
         public boolean change;
 
-        private DependencyEntry(boolean syntheticEnd, @NotNull Information<?> from, @NotNull Information<?> to) {
-            this.syntheticStart = false;
-            this.syntheticEnd = syntheticEnd;
+        private DependencyEntry(@NotNull DependencyNode from, @NotNull DependencyNode to) {
+            this.syntheticStart = from.isSynthetic();
             this.startType = from.getType();
             this.startPath = from.getPath();
             this.startName = from.getName();
+            this.syntheticEnd = to.isSynthetic();
             this.endType = to.getType();
             this.endPath = to.getPath();
             this.endName = to.getName();
-        }
-
-        @NotNull
-        private DependencyEntry(@NotNull Information<?> from, @NotNull DependencyEntry to) {
-            this.syntheticStart = true;
-            this.syntheticEnd = false;
-            this.startType = from.getType();
-            this.startPath = from.getPath();
-            this.startName = from.getName();
-            this.endType = to.endType;
-            this.endPath = to.endPath;
-            this.endName = to.endName;
         }
 
         @Override
@@ -132,6 +168,50 @@ public class DiffExtractor {
         @Override
         public String toString() {
             return startPath + (change ? '+' : '-') + '>' + endPath;
+        }
+
+        private static class DependencyNode {
+            private final boolean synthetic;
+            private final Information.Type type;
+            private final String path;
+            private final String name;
+
+            private DependencyNode(boolean synthetic, Information.Type type, String path, String name) {
+                this.synthetic = synthetic;
+                this.type = type;
+                this.path = path;
+                this.name = name;
+            }
+
+            private DependencyNode(boolean synthetic, @NotNull Information<?> info) {
+                this(synthetic, info.getType(), info.getPath(), info.getName());
+            }
+
+            /**
+             * Reconstructs a node from a dependency entry
+             *
+             * @param end   whether to create the start or the end node
+             * @param entry the Dependency entry to copy from
+             */
+            private DependencyNode(DependencyEntry entry, boolean end) {
+                this(end ? entry.syntheticEnd : entry.syntheticStart, end ? entry.endType : entry.startType, end ? entry.endPath : entry.startPath, end ? entry.endName : entry.startName);
+            }
+
+            public boolean isSynthetic() {
+                return synthetic;
+            }
+
+            public Information.Type getType() {
+                return type;
+            }
+
+            public String getPath() {
+                return path;
+            }
+
+            public String getName() {
+                return name;
+            }
         }
     }
 }
