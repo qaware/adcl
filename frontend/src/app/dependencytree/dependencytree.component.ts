45cddb10d4348433b90c0392384e0c1db1c49534
Bone9 <dani@fblan.de>
2020-02-19 02:44:33 +0100

    added is green and deleted is red

--- frontend/src/app/dependencytree/dependencytree.component.ts
+++ frontend/src/app/dependencytree/dependencytree.component.ts
@@ -632,11 +632,11 @@ export class DependencyTreeDatabase {
         });
 
         node.addedDependency.forEach(dep => {
-          edgeSet.push({from: node.id, to: dep.id, label: 'Added', arrows: 'to', color: '#000000', width: 3});
+          edgeSet.push({from: node.id, to: dep.id, label: 'Added', arrows: 'to', color: '#cc2222', width: 3});
         });
 
         node.deletedDependency.forEach(dep => {
-          edgeSet.push({from: node.id, to: dep.id, label: 'Deleted', arrows: 'to', color: '#000000', width: 3});
+          edgeSet.push({from: node.id, to: dep.id, label: 'Deleted', arrows: 'to', color: '#22cc22', width: 3});
         });
 
         nodeSet.push({id: node.id, label: node.name, title: node.tooltip, color: this.colorForType(node.type)});
